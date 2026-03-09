1b95576b94fbba37887412353b7d8735d5425f41
Ysch2305 <christianyosua112@gmail.com>
2026-03-09 15:20:30 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+TELEGRAM_TOKEN = "8505991544:AAFTAdCK-xlRZ1Oru9H7avi1fvw-kZ-TbJk"
+
+SCAN_INTERVAL = 1800  # 30 menit
+
+WATCHLIST = [
+    "BBCA.JK",
+    "BBRI.JK",
+    "BMRI.JK",
+    "TLKM.JK",
+    "ASII.JK",
+    "ADRO.JK",
+    "ICBP.JK",
+    "UNVR.JK",
+    "MDKA.JK",
+    "ANTM.JK"
+]
